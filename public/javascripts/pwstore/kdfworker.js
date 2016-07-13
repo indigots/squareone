ec2b59ada956c82a3e65d025dc84d879c8670cc1
Todd Switzer <toddswitzer@gmail.com>
2016-07-13 19:33:00 +0000

    Switched the key derivation to asmcrypto.js using hmac512.

--- public/javascripts/pwstore/kdfworker.js
+++ public/javascripts/pwstore/kdfworker.js
@@ -1,20 +1,26 @@
 /* Copyright (c) 2016 Todd Switzer */
-importScripts('/javascripts/sjcl/sjcl.js');
+importScripts('/javascripts/asmcrypto/asmcrypto.js');
 onmessage = function(event){
-  //console.log('starting...');
   var hexkey = doKDF(event.data.username, event.data.password);
-  //console.log(hexkey);
   toReturn = {
     encKey: hexkey.substring(0,64),
     signKey: hexkey.substring(64,96),
     passKey: hexkey.substring(96)
   }
+  console.log(toReturn.passKey.length);
   postMessage(toReturn);
 }
 
 function doKDF(username, password){
   var saltA = 'kbxYOlSNkLOqEZrji1Mch98WYsKop6Kia5Q82SfI0rFRSpMVd2LxnR5xyuTTk3Ao';
-  var salt =  username + password + saltA;
-  var hexkey = sjcl.misc.pbkdf2(password, salt, 100000, 512);
-  return(sjcl.codec.hex.fromBits(hexkey));
+  var salt =  username + saltA;
+  var outkey = asmCrypto.PBKDF2_HMAC_SHA512.hex(password, salt, 50005, 64);
+  return(outkey);
+  
 }
+
+function kdfVector(p, s, c, len){
+  var hexkey = asmCrypto.PBKDF2_HMAC_SHA512.hex(p, s, c, len);
+  return(outkey);
+}
+
